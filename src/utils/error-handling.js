797f96bb07693f670dc07d01084f64b0fea7e252
Fernando Dodino <fernando.dodino@gmail.com>
2022-11-17 18:49:18 -0300

    Adding network error handling

--- src/utils/error-handling.js
+++ src/utils/error-handling.js
@@ -1,9 +1,13 @@
+const INTERNAL_SERVER_ERROR = 500
+
+// TODO: hacer tests
+
 export const mostrarMensajeError = (error, setearMensaje) => {
-    const status = error.response.status
-    const mensajeError = status >= 500 ? 'Ocurrió un error. Consulte al administrador del sistema' :
-      error.response.data.message
-    if (status >= 500) {
-      console.error(error)
-    }
-    setearMensaje(mensajeError)
+  const status = error.response.status
+  const mensajeError = status >= INTERNAL_SERVER_ERROR ? 'Ocurrió un error. Consulte al administrador del sistema' :
+    (!status ? 'Ocurrió un error al conectarse al backend. Consulte al administrador del sistema' : error.response.data.message)
+  if (status >= INTERNAL_SERVER_ERROR) {
+    console.error(error)
   }
+  setearMensaje(mensajeError)
+}
