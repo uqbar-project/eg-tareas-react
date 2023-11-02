df5e2504488aa4bab89193f7cb796edde3820615
Fernando Dodino <fernando.dodino@gmail.com>
2023-11-02 00:18:51 -0300

    fix test

--- src/utils/error-handling.jsx
+++ src/utils/error-handling.jsx
@@ -3,9 +3,9 @@ const INTERNAL_SERVER_ERROR = 500
 // TODO: hacer tests
 
 export const mostrarMensajeError = (error, setearMensaje) => {
-  const status = error.response.status
+  const status = error.response?.status
   const mensajeError = status >= INTERNAL_SERVER_ERROR ? 'Ocurrió un error. Consulte al administrador del sistema' :
-    (!status ? 'Ocurrió un error al conectarse al backend. Consulte al administrador del sistema' : error.response.data.message)
+    !status ? 'Ocurrió un error al conectarse al backend. Consulte al administrador del sistema' : error.response.data.message
   if (status >= INTERNAL_SERVER_ERROR) {
     console.error(error)
   }
