b6b3683126bd27d8683ab342961d1eca7f0f34ac
Fernando Dodino <fernando.dodino@gmail.com>
2024-10-30 18:41:06 -0300

    Update coverage

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     environment: 'jsdom',
     coverage: {
       reporter: ['lcov', 'json', 'html', 'json-summary'],
+      exclude: [ './src/main.tsx', '**/*.cjs', '**/*.d.ts', 'vite.config.ts' ],      
     },
   }
 })
